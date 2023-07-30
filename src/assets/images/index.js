06a75a255dee5b74249ed80c1af24a35ce677455
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-07-30 08:55:56 +0700

    create homepage

---
+++ src/assets/images/index.js
@@ -0,0 +1,1 @@
+export { default as HomePageBackground } from "homepageBackground.jpg";
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Layout from "../../components/Layout";
 
+import "./index.css";
+
 export default function Homepage() {
   return (
-    <Layout>
-      <div className="h-[100vh] bg-secondary"></div>
-    </Layout>
+    <>
+      <Layout>
+        <div className="bg-secondary min-h-[89.2vh] text-white homepage">
+          <div className="flex flex-col justify-center items-center h-[89vh] text">
+            <h1 className="text-center font-bold text-5xl">Welcome, to Isekai School</h1>
+            <p className="text-center mt-8">
+              in this website, you can see all the detail classes, who the students will <span className="text-primary font-bold">die</span> this months, and who student who <span className="text-pink-600 font-bold">marry</span>
+            </p>
+          </div>
+        </div>
+      </Layout>
+    </>
   );
 }
