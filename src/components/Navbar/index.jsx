9a748a4101b8c589db9d4e9eb762dc11c32ffd83
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-07-30 22:28:17 +0700

    install react-icons

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,15 +3,40 @@ import { Link } from "react-router-dom";
 
 import "./index.css";
 
+import { GiHamburgerMenu } from "react-icons/gi";
+
 export default function Navbar() {
   return (
     <nav className="font-sans flex flex-col text-left sm:flex-row sm:text-left sm:justify-between py-6 px-10 w-[100%] bg-secondary text-white">
-      <div className="mb-2 sm:mb-0">
-        <Link href="/" className="text-2xl font-bold text-primary">
-          Home
+      <div className="">
+        <Link href="/" className="text-3xl font-bold text-primary">
+          Isescholl
         </Link>
       </div>
-      <ul className="flex flex-col sm:flex-row justify-end sm:gap-10 w-full">
+      <ul className="hidden md:flex md:flex-row md:justify-end md:gap-10 md:w-full">
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/class-detail"}>Classes</Link>
+        </li>
+        <li>
+          <Link to={"/student-will-die"}>Student Who Will Die</Link>
+        </li>
+        <li>
+          <Link to={"/student-will-marry"}>Student Who Will Marry</Link>
+        </li>
+      </ul>
+
+      {/* Responsive */}
+      <div>
+        <Link href="/" className="text-3xl font-bold text-primary">
+          Isescholl
+        </Link>
+
+        <GiHamburgerMenu />
+      </div>
+      <ul className="md:hidden sm:flex sm:flex-col sm:gap-10 py-3">
         <li>
           <Link to={"/"}>Home</Link>
         </li>
